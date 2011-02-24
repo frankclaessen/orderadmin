d3b5d0d5c58158e848d514a3e3065c7be3843b5c
frankclaessen <claessen.frank@gmail.com>
2011-02-24 19:49:57 +0100

    after rails create

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OrderAdmin::Application.config.secret_token = 'c713ac6f623ae2ccd6d82dcb13d46990a48d11481b5290cef252c8c272e4e9e87068a0a9e253a569d4258158e30eb632c013012cbc04f8a25b347aced63003f5'
